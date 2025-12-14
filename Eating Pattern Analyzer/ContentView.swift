deee3bfd702172b1bf5089da245dd703f45d64d6
Ansh Singh <s.ansh3007@gmail.com>
2025-12-14 01:49:54 -0600

    Created a data model for the app.

--- Eating Pattern Analyzer/ContentView.swift
+++ Eating Pattern Analyzer/ContentView.swift
@@ -22,3 +22,31 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+// This is my first swift app, so some comments may seem obvious, but they are only here for my reference & for my learning.
+
+// Identifiable protocol indicates that each struct instance has a unique id
+// Codable protocol makes conversion to external data formats easy
+struct Meal: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    let foods: [FoodItem]
+    let tags: [MealTag]
+}
+
+struct FoodItem: Identifiable, Codable {
+    let id: UUID
+    let name: String
+    let quantity: String
+}
+
+// String is interestingly not a protocol, but a raw-value type. Confusing how it's listed with the other protocols.
+// CaseIterable protocol indicates that enum can iterate over all cases with: MealTag.allCases
+enum MealTag: String, Codable, CaseIterable {
+    case snack
+    case meal
+    case lateNight
+    case stress
+    case social
+    case healthy
+}
---
+++ Eating Pattern Analyzer/Preview Content/second.swift
@@ -0,0 +1,8 @@
+//
+//  second.swift
+//  Eating Pattern Analyzer
+//
+//  Created by Anshdeep Singh on 12/14/25.
+//
+
+import Foundation
