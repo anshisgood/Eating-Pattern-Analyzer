ad1135e90e2e7f70f6030cbceedc4f11cf0b41ae
Ansh Singh <s.ansh3007@gmail.com>
2025-12-14 00:06:53 -0600

    Initial Commit

---
+++ Eating Pattern Analyzer/Eating_Pattern_AnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eating_Pattern_AnalyzerApp.swift
+//  Eating Pattern Analyzer
+//
+//  Created by Anshdeep Singh on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Eating_Pattern_AnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Eating Pattern AnalyzerTests/Eating_Pattern_AnalyzerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Eating_Pattern_AnalyzerTests.swift
+//  Eating Pattern AnalyzerTests
+//
+//  Created by Anshdeep Singh on 12/13/25.
+//
+
+import Testing
+@testable import Eating_Pattern_Analyzer
+
+struct Eating_Pattern_AnalyzerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
